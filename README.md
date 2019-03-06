d9ddcb48bf339fc4ef604626e54be135e484d9e5
Smirk <35244986+SmirkCao@users.noreply.github.com>
2019-03-06 22:29:28 +0800

    optimization, TFDetector calss and ... (#1)

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# v0.1.0
+# Object Detection Tutorial
+## v0.1.0
 
-## Update
+### Update
 
 - draw bbox and label
 - multi images
 
-## Usage
+### Usage
 
 - download model from https://github.com/tensorflow/models/blob/v1.13.0/research/object_detection/g3doc/detection_model_zoo.md
 - run `python object_detection_tutorial.py --model_frozen /PATH/TO/MODEL`
@@ -15,9 +16,17 @@ for example:
 python3 object_detection_tutorial.py --model_frozen ../ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb
 ```
 
-## ref:
+### ref:
 - modify from tensorflow detection api demo(v1.13.0): https://github.com/tensorflow/models/blob/v1.13.0/research/object_detection/object_detection_tutorial.ipynb
 
-v0.1.1
+## v0.1.1
 - optimization
 - TFDetector class
+
+### Update
+- new TFDetector class with session as class member
+- remove PIL, matplotlib dependencies
+- reformat the code
+- combine load image paths and labels in load_image_and_labels functions
+- remove isimage function, add types parameter to handle more image types in the future
+- remove main function 
--- object_detection_tutorial.py
+++ object_detection_tutorial.py
@@ -1,130 +1,134 @@
+from distutils.version import StrictVersion
+from label_utils import label_map_util
+
 import numpy as np
 import tensorflow as tf
 import argparse
-import sys
 import os
 import time
 import cv2
 
-from distutils.version import StrictVersion
-from PIL import Image
-from matplotlib import pyplot as plt
-
 FLAGS = None
 
 if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
-  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
-
-from label_utils import label_map_util
-
-def isimage(path):
-  return os.path.splitext(path)[1].lower() in ['.jpg', '.png', '.jpeg']
-
-def create_graph(model_path):
-  detection_graph = tf.Graph()
-  with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(model_path, 'rb') as fid:
-      serialized_graph = fid.read()
-      od_graph_def.ParseFromString(serialized_graph)
-      tf.import_graph_def(od_graph_def, name='')
-  return detection_graph
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
-def run_inference_for_single_image(image, graph):
-  with graph.as_default():
-    with tf.Session() as sess:
-      # Get handles to input and output tensors
-      ops = tf.get_default_graph().get_operations()
-      all_tensor_names = {output.name for op in ops for output in op.outputs}
-      tensor_dict = {}
-      for key in [
-          'num_detections', 'detection_boxes', 'detection_scores',
-          'detection_classes', 'detection_masks'
-      ]:
-        tensor_name = key + ':0'
-        if tensor_name in all_tensor_names:
-          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-              tensor_name)
-
-      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
-      # Run inference
-      start_time = time.time()
-      output_dict = sess.run(tensor_dict,
-                             feed_dict={image_tensor: np.expand_dims(image, 0)})
-      end_time = time.time()
-      print("run time:", end_time - start_time)
-
-      # all outputs are float32 numpy arrays, so convert types as appropriate
-      num = int(output_dict['num_detections'][0])
-      output_dict['num_detections'] = num
-      output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)[:num]
-      output_dict['detection_boxes'] = output_dict['detection_boxes'][0][:num]
-      output_dict['detection_scores'] = output_dict['detection_scores'][0][:num]
-
-  return output_dict
-
-def main():
-  # labels
-  category_index = label_map_util.create_category_index_from_labelmap(FLAGS.label_path, use_display_name=True)
-
-  # create graph
-  detection_graph = create_graph(FLAGS.model_frozen)
-
-  image_paths = []
-  if os.path.isfile(FLAGS.image_path):
-    image_paths.append(FLAGS.image_path)
-  else:
-    for file_or_dir in os.listdir(FLAGS.image_path):
-      file_path = os.path.join(FLAGS.image_path, file_or_dir)
-      if os.path.isfile(file_path) and isimage(file_path):
-        image_paths.append(file_path)
-  print(image_paths)
+    raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
 
-  for image_path in image_paths:
-    # prepare data
-    image = Image.open(image_path)
-    (image_width, image_height) = image.size
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
+def load_image_and_labels(label_path,
+                          image_path,
+                          verbose=True,
+                          types=['.jpg', '.png', '.jpeg']):
+    # labels
+    category_index = label_map_util.create_category_index_from_labelmap(label_path, use_display_name=True)
+    
+    image_paths = []
+    if os.path.isfile(image_path):
+        image_paths.append(image_path)
+    else:
+        for file_or_dir in os.listdir(image_path):
+            file_path = os.path.join(image_path, file_or_dir)
+            if os.path.isfile(file_path) and \
+                    os.path.splitext(file_path)[1].lower() in types:
+                image_paths.append(file_path)
+    if verbose:
+        print(image_paths)
+    return category_index, image_paths
 
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
 
-    for i in range(output_dict['num_detections']):
-      cls_id = output_dict['detection_classes'][i]
-      cls_name = category_index[cls_id]['name']
-      score = output_dict['detection_scores'][i]
-      box_ymin = int(output_dict['detection_boxes'][i][0]*image_height)
-      box_xmin = int(output_dict['detection_boxes'][i][1]*image_width)
-      box_ymax = int(output_dict['detection_boxes'][i][2]*image_height)
-      box_xmax = int(output_dict['detection_boxes'][i][3]*image_width)
-      cv2.rectangle(image_np, (box_xmin,box_ymin), (box_xmax,box_ymax), (0,255,0),3)
-      text = "%s:%.2f" % (cls_name,score)
-      cv2.putText(image_np, text, (box_xmin,box_ymin-4),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (255,0,0))
-
-    plt.figure(figsize=(12, 8)) # Size, in inches
-    plt.imshow(image_np)
-    plt.show()
+class TFDetector(object):
+    def __init__(self, model_path, category_index):
+        self.graph = self.create_graph(model_path)
+        self.sess = self.create_session()
+        self.category_index = category_index
+    
+    def create_graph(self, model_path):
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        self.graph = detection_graph
+        return self.graph
+    
+    def create_session(self):
+        with self.graph.as_default():
+            self.sess = tf.Session()
+        return self.sess
+    
+    def detect(self, image, mark=False):
+        with self.graph.as_default():
+            # Get handles to input and output tensors
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in [
+                'num_detections', 'detection_boxes', 'detection_scores',
+                'detection_classes', 'detection_masks'
+            ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
+            
+            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+            # Run inference
+            start_time = time.time()
+            output_dict = self.sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
+            end_time = time.time()
+            print("run time:", end_time - start_time)
+            
+            # all outputs are float32 numpy arrays, so convert types as appropriate
+            num = int(output_dict['num_detections'][0])
+            output_dict['num_detections'] = num
+            output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)[:num]
+            output_dict['detection_boxes'] = output_dict['detection_boxes'][0][:num]
+            output_dict['detection_scores'] = output_dict['detection_scores'][0][:num]
+            
+            if mark:
+                for i in range(output_dict['num_detections']):
+                    image_height, image_width = image.shape[:2]
+                    
+                    cls_id = output_dict['detection_classes'][i]
+                    cls_name = self.category_index[cls_id]['name']
+                    score = output_dict['detection_scores'][i]
+                    box_ymin = int(output_dict['detection_boxes'][i][0] * image_height)
+                    box_xmin = int(output_dict['detection_boxes'][i][1] * image_width)
+                    box_ymax = int(output_dict['detection_boxes'][i][2] * image_height)
+                    box_xmax = int(output_dict['detection_boxes'][i][3] * image_width)
+                    cv2.rectangle(image, (box_xmin, box_ymin), (box_xmax, box_ymax), (0, 255, 0), 3)
+                    text = "%s:%.2f" % (cls_name, score)
+                    cv2.putText(image, text, (box_xmin, box_ymin - 4), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8,
+                                (255, 0, 0))
+                # show image
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                cv2.imshow("img", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+        return output_dict, image
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--image_path', type=str,
-                      default='test_images/',
-                      help='image path')
-  parser.add_argument('--model_frozen', type=str,
-                      default='/home/andy/selfdrivingcar/TFMODEL/ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb',
-                      help='model path')
-  parser.add_argument('--label_path', type=str,
-                      default='label_utils/mscoco_label_map.pbtxt',
-                      help='label path')
-  FLAGS, unparsed = parser.parse_known_args()
-  main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str,
+                        default='test_images/',
+                        help='image path')
+    parser.add_argument('--model_frozen', type=str,
+                        default='TFMODEL/ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb',
+                        help='model path')
+    parser.add_argument('--label_path', type=str,
+                        default='label_utils/mscoco_label_map.pbtxt',
+                        help='label path')
+    FLAGS, unparsed = parser.parse_known_args()
+    
+    category_index, image_paths = load_image_and_labels(FLAGS.label_path, FLAGS.image_path)
+    detector = TFDetector(FLAGS.model_frozen, category_index)
+    
+    for image_path in image_paths:
+        # load image
+        image = cv2.imread(image_path)
+        # convert color space, try to remove this you'll see amazing result for image1
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # detection
+        detector.detect(image, mark=True)
